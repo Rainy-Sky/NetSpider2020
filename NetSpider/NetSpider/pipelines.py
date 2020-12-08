f63851f0141ce99ff60bf3db2f497856ee3c5c92
Rainy-Sky <54521443+Rainy-Sky@users.noreply.github.com>
2020-12-08 17:39:52 +0800

    update

--- NetSpider/NetSpider/pipelines.py
+++ NetSpider/NetSpider/pipelines.py
@@ -8,19 +8,26 @@ import json
 import codecs
 import os
 import time
+from scrapy import signals
+from pydispatch import dispatcher
 
 class NetspiderPipeline(object):
     def __init__(self):  # 初始化，打开文件
+        dispatcher.connect(self.spider_closed, signals.spider_closed)
         local_time = time.strftime("%Y_%m_%d", time.localtime())
         file_path = './data/'+local_time+'.json'
         if os.path.exists(file_path):
             os.remove(file_path)
-        self.file = codecs.open(file_path, 'a+', encoding="utf-8")
+        self.file = codecs.open(file_path, 'w', encoding="utf-8")
         # 这里用codecs库来打开文件，目的是编码不会出错
+        self.file.write("{\"Train\":[\n")
 
     def process_item(self, item, spider):  # 写入文件
-        lines = json.dumps(dict(item), ensure_ascii=False) + "\n"
+        lines ="\t" + json.dumps(dict(item), ensure_ascii=False) + ",\n"
         self.file.write(lines)
         return item
+
     def spider_closed(self, spider):  # 关闭文件
+        self.file.seek(-2,1)
+        self.file.write("]\n}")
         self.file.close()
