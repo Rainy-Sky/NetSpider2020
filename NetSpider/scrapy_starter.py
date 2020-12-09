376381025a711eb030d42f6a06c8cab23bc9b0c2
rainy-Sky <yty18373729@buaa.edu.cn>
2020-12-09 09:05:29 +0800

    update

--- NetSpider/scrapy_starter.py
+++ NetSpider/scrapy_starter.py
@@ -4,8 +4,8 @@ import time
 if __name__ == '__main__':
     # os.system('pwd')
     while True:
-        os.system("scrapy crawl Train")
+        os.system("scrapy crawl Train --nolog")
         print("start")
-        # 每1２个小时执行一次　６０＊６０＊２
+        # 每1２个小时执行一次　６０＊６０＊1２
 
         time.sleep(60*60*12)
--- README.md
+++
@@ -1,1 +0,0 @@
-# NetSpider2020
